222dd4b7e92b6034b4e2602670b28f41faf507e0
HindAlmorshed <81118373+HindAlmorshed@users.noreply.github.com>
2024-05-21 14:24:18 +0200

    up

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,23 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+
+            $table->string('state')->nullable();
+            $table->string('job')->nullable();
+            $table->string('base_pay')->nullable();
+            $table->string('overtime_pay')->nullable();
+            $table->string('total_pay')->nullable();
+            $table->string('birthday')->nullable();
+            $table->string('num_family')->nullable();
+            $table->string('address')->nullable();
+
+            $table->foreignId('role_id')->nullable();
+            $table->foreignId('center_id')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });

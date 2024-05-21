21439ec1555f643c0751ef69f6660d56c2ed1141
HindAlmorshed <81118373+HindAlmorshed@users.noreply.github.com>
2024-05-21 15:49:46 +0200

    up

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,6 +8,7 @@ use App\Models\User_Project;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,10 @@ Route::group([
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
+    // Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+    // Route::get('/user-profile', [AuthController::class, 'userProfile']);    
 });
 
 

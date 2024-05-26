2860a9a9163de478eaf43d27512091dd17d5884d
ramaShahwan <rama692000@gmail.com>
2024-05-26 16:45:41 +0300

    up

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use Validator;
+// use Validator;
 
 class AuthController extends Controller
 {
